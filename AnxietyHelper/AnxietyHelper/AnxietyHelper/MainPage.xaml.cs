66d4a25f88d1c63b06c8e21dd1a18744ea511c1a
Connor Keyes <connor.o.keyes@ou.edu>
2019-04-13 12:54:42 -0500

    Created Project

---
+++ AnxietyHelper/AnxietyHelper/AnxietyHelper/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿//AnxietyHelper
+//Connor Keyes, Hayley Hunter, James Nelson
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AnxietyHelper
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
