96ae32f744f0413b5195ae488c36a0ed0eecb7bc
Connor Keyes <connor.o.keyes@ou.edu>
2019-04-18 18:40:49 -0500

    Made another project with a different xamarin form

---
+++ AnxietyHelper2.0/AnxietyHelper2.0/AnxietyHelper2.0/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using AnxietyHelper2._0.Models;
+
+namespace AnxietyHelper2._0.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ AnxietyHelper2.0/AnxietyHelper2.0/AnxietyHelper2.0/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using AnxietyHelper2._0.Models;
+using AnxietyHelper2._0.ViewModels;
+
+namespace AnxietyHelper2._0.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ItemDetailPage : ContentPage
+    {
+        ItemDetailViewModel viewModel;
+
+        public ItemDetailPage(ItemDetailViewModel viewModel)
+        {
+            InitializeComponent();
+
+            BindingContext = this.viewModel = viewModel;
+        }
+
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+
+            var item = new Item
+            {
+                Text = "Item 1",
+                Description = "This is an item description."
+            };
+
+            viewModel = new ItemDetailViewModel(item);
+            BindingContext = viewModel;
+        }
+    }
+}
